b982a03a28a8be985e8a7955c89ea7b01dda5ab3
philmcqueid <philmcqueid@gmail.com>
2022-02-09 14:05:32 -0300

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.blue.shade900,
-      appBar: AppBar(
-        backgroundColor: Colors.blue.shade900,
-        elevation: 0.0,
-        title: const Center(child: Text('Me pergunte qualquer coisa')),
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo.shade900,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          elevation: 0.0,
+          title: const Center(child: Text('Ask me anything')),
+        ),
+        body: const MagicBalls(),
       ),
-      body: const MagicBalls(),
-    ),
-  ));
+    );
+  }
 }
 
 class MagicBalls extends StatefulWidget {
@@ -22,8 +32,23 @@ class MagicBalls extends StatefulWidget {
 }
 
 class _MagicBallsState extends State<MagicBalls> {
+  int ballNumber = 1;
+
+  void changeBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello');
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          changeBall();
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
   }
 }
